c69f8816dd200b634e3a8239d773f10fcfbed1d4
ismael-albaiti <ismael.albaiti@gmail.com>
2025-11-17 13:22:15 +0300

    setting argparse

---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+# import sys
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    _ = parser.add_argument("message", help="message to send to the agent")
+    _ = parser.add_argument(
+        "-v", "--verbose", action="store_true", help="enable verbose output"
+    )
+
+    args = parser.parse_args()
+
+    _ = load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt: str = args.message
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(  # pyright: ignore[reportUnknownMemberType]
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")  # pyright: ignore[reportOptionalMemberAccess]
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  # pyright: ignore[reportOptionalMemberAccess]
+
+
+if __name__ == "__main__":
+    main()
